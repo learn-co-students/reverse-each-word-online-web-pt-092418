7ff9a8667e076218c083df58b096e9dd284baa3c
Emilio Betancourt <embeor@gmail.com>
2018-10-05 03:10:46 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
+
+
 def reverse_each_word (string)
   string_reverse = []
   split_string = string.split(" ")
-    split_string.each do |word|
+    split_string.collect do |word|
       string_reverse << word.reverse
     end
   string_reverse.join (" ")
